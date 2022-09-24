d577173fbb85964eaa6ad16dcece3a1f89374304
Anthony Gorski <anthony.developpeurweb@gmail.com>
2022-09-24 22:44:52 +0200

    template done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import Layout from "@pages/Layout";
+import Navbar from "@components/Navbar";
+import Footer from "@components/Footer";
+
 const App = () => {
   return (
-    <div>
-      <h1>My App</h1>
-      <p>My App is awesome!</p>
-    </div>
+    <main className="flex flex-col h-screen">
+      <Navbar />
+      <Layout>
+        <h1>My App</h1>
+        <p>My App is awesome!</p>
+      </Layout>
+      <Footer />
+    </main>
   );
 };
 
