0199d1402ee8f194c3d8ffffb1a461e649c1bd85
Anthony Gorski <anthony.developpeurweb@gmail.com>
2022-10-12 00:08:01 +0200

    modified: week

--- src/components/Weeks.jsx
+++ src/components/Weeks.jsx
@@ -6,7 +6,7 @@ export default function Weeks() {
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-wrap -mx-4 -my-8">
+        <div className="flex flex-wrap justify-center md:justify-start -mx-4 -my-8">
           {meals.map((meal) => (
             <Card key={meal.id} meal={meal} />
           ))}
