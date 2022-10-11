a070342e309df69bcd15220f46df92acc8332a37
Anthony Gorski <anthony.developpeurweb@gmail.com>
2022-10-11 20:59:06 +0200

    calendar done

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -6,19 +6,12 @@ export default function MyCalendar() {
   const [date, setDate] = useState(new Date());
 
   const onChange = (newDate) => {
-    console.log(`New date selected - ${newDate.toString()}`);
     setDate(newDate);
   };
 
   return (
     <>
-      <DatePicker
-        onChange={onChange}
-        value={date}
-        weekStartsFrom="Monday"
-        lang={"fr"}
-      />
-      <p>{date.toString()}</p>
+      <DatePicker onChange={onChange} value={date} weekStartsFrom="Monday" />
     </>
   );
 }
